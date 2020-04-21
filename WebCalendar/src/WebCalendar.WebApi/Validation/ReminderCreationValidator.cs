c8f64bb4de7e245989b83c70da05e9269178f4d9
buyBuzz <49615714+buyBuzz@users.noreply.github.com>
2020-04-21 03:15:57 +0300

    empty validators

---
+++ WebCalendar/src/WebCalendar.WebApi/Validation/ReminderCreationValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCalendar.Services.Models.Reminder;
+
+namespace WebCalendar.WebApi.Validation
+{
+    public class ReminderCreationValidator
+    {
+    }
+}
---
+++ WebCalendar/src/WebCalendar.WebApi/Validation/TaskCreationValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCalendar.WebApi.Models.Task;
+
+namespace WebCalendar.WebApi.Validation
+{
+    public class TaskCreationValidator : AbstractValidator<TaskCreationModel>
+    {
+    }
+}
