e682d1cee3002a4aa85f13f66505110131b1ec44
buyBuzz <49615714+buyBuzz@users.noreply.github.com>
2020-04-21 03:15:57 +0300

    empty validators

---
+++ WebCalendar/src/WebCalendar.WebApi/Validation/UserRegistrationValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCalendar.Services.Models.User;
+
+namespace WebCalendar.WebApi.Validation
+{
+    public class UserRegistrationValidator : AbstractValidator<UserRegisterServiceModel>
+    {
+
+    }
+}
