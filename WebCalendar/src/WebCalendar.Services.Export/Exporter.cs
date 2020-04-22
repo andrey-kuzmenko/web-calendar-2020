137dbe89e3cdf963b7ea5b9b7d671ce2060ed5d2
buyBuzz <49615714+buyBuzz@users.noreply.github.com>
2020-04-22 23:51:04 +0300

    wtf

--- WebCalendar/src/WebCalendar.Services.Export/Exporter.cs
+++ WebCalendar/src/WebCalendar.Services.Export/Exporter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Ical.Net.CalendarComponents;
 //using Ical.Net;
 using Ical.Net.DataTypes;
 //using Ical.Net.Serialization.iCalendar.Serializers;
@@ -16,15 +18,13 @@ namespace WebCalendar.Services.Export
         
         public async void ExportCalendar(Guid id)
         {
-               Calendar calendar = await _uow.GetRepository<Calendar>()
-                .GetFirstOrDefaultAsync(
-                    predicate: c => c.Id == id, 
-                    include: source => source.Include(c => c.Events)
-                        .ThenInclude(e => e.Event));
-            Ical.Net.Calendar icalenadar = new Ical.Net.Calendar();
+            Calendar calendar = await _uow.GetRepository<Calendar>()
+             .GetFirstOrDefaultAsync(
+                 predicate: c => c.Id == id,
+                 include: source => source.Include(c => c.Events)
+                    .Include(c => c.Tasks));
 
-
-
+            Ical.Net.Calendar iCalenadar = new Ical.Net.Calendar();
 
         }
     }
---
+++ WebCalendar/src/WebCalendar.Services.Export/Mapper/ExportEventModelProfile.cs
@@ -0,0 +1,25 @@
+﻿using Ical.Net.CalendarComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCalendar.Common;
+using WebCalendar.DAL.Models.Entities;
+
+namespace WebCalendar.Services.Export.Mapper
+{
+    class ExportEventModelProfile : AutoMapperProfile
+    {
+        public ExportEventModelProfile()
+        {
+            CreateMap<Event, CalendarEvent>()
+                   .ForMember(e => e.Uid, o => o.MapFrom(e => e.Id))
+                   .ForMember(e => e.Name, o => o.MapFrom(e => e.Title))
+                   .ForMember(e => e.Description, o => o.MapFrom(e => e.Description))
+                   .ForMember(e => e.Location, o => o.MapFrom(e => e.Location))
+                   .ForMember(e => e.Created, o => o.MapFrom(e => e.AddedDate))
+                   .ForMember(e => e.LastModified, o => o.MapFrom(e => e.ModifiedDate))
+                   .ForMember(e => e.Start, o => o.MapFrom(e => e.StartTime))
+                   .ForMember(e => e.End, o => o.MapFrom(e => e.EndTime));
+        }
+    }
+}
