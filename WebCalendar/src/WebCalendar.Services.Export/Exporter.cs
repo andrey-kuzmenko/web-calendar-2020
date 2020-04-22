8a351af4d0789587a479e4f5bdb129452a7114ed
buyBuzz <49615714+buyBuzz@users.noreply.github.com>
2020-04-22 23:49:46 +0300

    add project and automapper

---
+++ WebCalendar/src/WebCalendar.Services.Export/Exporter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+//using Ical.Net;
+using Ical.Net.DataTypes;
+//using Ical.Net.Serialization.iCalendar.Serializers;
+using Ical.Net.Serialization;
+using Microsoft.EntityFrameworkCore;
+using WebCalendar.DAL;
+using WebCalendar.DAL.Models.Entities;
+
+namespace WebCalendar.Services.Export
+{
+    public class Exporter
+    {
+        private readonly IUnitOfWork _uow;
+        
+        public async void ExportCalendar(Guid id)
+        {
+               Calendar calendar = await _uow.GetRepository<Calendar>()
+                .GetFirstOrDefaultAsync(
+                    predicate: c => c.Id == id, 
+                    include: source => source.Include(c => c.Events)
+                        .ThenInclude(e => e.Event));
+            Ical.Net.Calendar icalenadar = new Ical.Net.Calendar();
+
+
+
+
+        }
+    }
+}
