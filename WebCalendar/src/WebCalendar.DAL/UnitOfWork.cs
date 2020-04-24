735433b8a7bffdb0c6b3a2d7587a3a734ce45837
buyBuzz <49615714+buyBuzz@users.noreply.github.com>
2020-04-24 06:51:01 +0300

    remove xml

--- WebCalendar/src/WebCalendar.DAL/UnitOfWork.cs
+++ WebCalendar/src/WebCalendar.DAL/UnitOfWork.cs
@@ -15,38 +15,17 @@ using WebCalendar.DAL.Repositories.Implementation;
 
 namespace WebCalendar.DAL
 {
-    /// <summary>
-    /// Represents the default implementation of the <see cref="IUnitOfWork"/> and <see cref="IUnitOfWork{TContext}"/> interface.
-    /// </summary>
-    ///// <typeparam name="TContext">The type of the db context.</typeparam>
     public class UnitOfWork : IAsyncRepositoryFactory, IUnitOfWork
     {
         private readonly ApplicationDbContext _context;
         private bool disposed = false;
         private Dictionary<Type, object> repositories;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UnitOfWork/*{TContext}*/"/> class.
-        /// </summary>
-        /// <param name="context">The context.</param>
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        ///// <summary>
-        ///// Gets the db context.
-        ///// </summary>
-        ///// <returns>The instance of type <typeparamref name="Context"/>.</returns>
-        //public TContext DbContext => _context;
-
-        /// <summary>
-        /// Changes the database name. This require the databases in the same machine. NOTE: This only work for MySQL right now.
-        /// </summary>
-        /// <param name="database">The database name.</param>
-        /// <remarks>
-        /// This only been used for supporting multiple databases in the same model. This require the databases in the same machine.
-        /// </remarks>
         public void ChangeDatabase(string database)
         {
             var connection = _context.Database.GetDbConnection();
@@ -71,12 +50,6 @@ namespace WebCalendar.DAL
             }
         }
 
-        /// <summary>
-        /// Gets the specified repository for the <typeparamref name="TEntity"/>.
-        /// </summary>
-        /// <param name="hasCustomRepository"><c>True</c> if providing custom repositry</param>
-        /// <typeparam name="TEntity">The type of the entity.</typeparam>
-        /// <returns>An instance of type inherited from <see cref="IRepository{TEntity}"/> interface.</returns>
         public IAsyncRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class
         {
             if (repositories == null)
@@ -103,28 +76,10 @@ namespace WebCalendar.DAL
             return (IAsyncRepository<TEntity>)repositories[type];
         }
 
-        /// <summary>
-        /// Executes the specified raw SQL command.
-        /// </summary>
-        /// <param name="sql">The raw SQL.</param>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>The number of state entities written to database.</returns>
         public int ExecuteSqlCommand(string sql, params object[] parameters) => _context.Database.ExecuteSqlRaw(sql, parameters);
 
-        /// <summary>
-        /// Uses raw SQL queries to fetch the specified <typeparamref name="TEntity" /> data.
-        /// </summary>
-        /// <typeparam name="TEntity">The type of the entity.</typeparam>
-        /// <param name="sql">The raw SQL.</param>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>An <see cref="IQueryable{T}" /> that contains elements that satisfy the condition specified by raw SQL.</returns>
         public IQueryable<TEntity> FromSql<TEntity>(string sql, params object[] parameters) where TEntity : class => _context.Set<TEntity>().FromSqlRaw(sql, parameters);
 
-        /// <summary>
-        /// Saves all changes made in this context to the database.
-        /// </summary>
-        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
-        /// <returns>The number of state entries written to the database.</returns>
         public int SaveChanges(bool ensureAutoHistory = false)
         {
             //if (ensureAutoHistory)
@@ -135,11 +90,6 @@ namespace WebCalendar.DAL
             return _context.SaveChanges();
         }
 
-        /// <summary>
-        /// Asynchronously saves all changes made in this unit of work to the database.
-        /// </summary>
-        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
-        /// <returns>A <see cref="Task{TResult}"/> that represents the asynchronous save operation. The task result contains the number of state entities written to database.</returns>
         public async Task<int> SaveChangesAsync(bool ensureAutoHistory = false)
         {
             //if (ensureAutoHistory)
@@ -150,12 +100,6 @@ namespace WebCalendar.DAL
             return await _context.SaveChangesAsync();
         }
 
-        /// <summary>
-        /// Saves all changes made in this context to the database with distributed transaction.
-        /// </summary>
-        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
-        /// <param name="unitOfWorks">An optional <see cref="IUnitOfWork"/> array.</param>
-        /// <returns>A <see cref="Task{TResult}"/> that represents the asynchronous save operation. The task result contains the number of state entities written to database.</returns>
         public async Task<int> SaveChangesAsync(bool ensureAutoHistory = false, params IUnitOfWork[] unitOfWorks)
         {
             using (var ts = new TransactionScope())
@@ -174,9 +118,6 @@ namespace WebCalendar.DAL
             }
         }
 
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
         public void Dispose()
         {
             Dispose(true);
@@ -184,10 +125,6 @@ namespace WebCalendar.DAL
             GC.SuppressFinalize(this);
         }
 
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
-        /// <param name="disposing">The disposing.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!disposed)
